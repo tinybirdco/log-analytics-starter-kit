06064863da4316275bad9bbc780e1ee37782943e
Dmytro Radchuk <dmytro@tinybird.co>
2022-12-13 13:23:57 +0100

    FIx babel error Add vitest

---
+++ dashboard/vitest.config.js
@@ -0,0 +1,14 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+  },
+})
---
+++ dashboard/vitest.setup.ts
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom/extend-expect'
+import { beforeAll, afterAll, afterEach } from 'vitest'
+import { server } from './mocks/server'
+
+beforeAll(() => {
+  window.location = {
+    search: '?token=my_token&host=https://analytics-api.com',
+  } as any
+  server.listen({ onUnhandledRequest: 'error' })
+})
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
