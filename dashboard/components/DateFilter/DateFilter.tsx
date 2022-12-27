8fa27f273623864c4dab92dc4fbbb07ea819f79a
Dmytro Radchuk <dmytro@tinybird.co>
2022-12-27 12:45:01 +0100

    Add link to GH in credentials modal Add GH "Star" to header Fix popover styles

--- dashboard/components/DateFilter/DateFilter.tsx
+++ dashboard/components/DateFilter/DateFilter.tsx
@@ -27,13 +27,21 @@ export default function DateFilter() {
 
   return (
     <div className="flex items-center gap-4">
-      <Popover className="relative">
+      <iframe
+        // TODO: add &count=true, change width to 170
+        src="https://ghbtns.com/github-btn.html?user=tinybirdco&repo=log-analytics-starter-kit&type=star&size=large"
+        width="75"
+        height="30"
+        title="GitHub"
+      />
+
+      <Popover className="relative h-4">
         <Popover.Button>
           <QuestionIcon className="text-secondaryLight" />
           <div className="sr-only">What is the time zone used?</div>
         </Popover.Button>
 
-        <Popover.Panel className="absolute bg-secondary text-white text-xs font-light rounded py-1 px-2 z-[2]">
+        <Popover.Panel className="absolute bottom-6 -right-10 bg-secondary text-white text-xs font-light rounded py-1 px-2 z-[2] w-24">
           UTC timezone
         </Popover.Panel>
       </Popover>
